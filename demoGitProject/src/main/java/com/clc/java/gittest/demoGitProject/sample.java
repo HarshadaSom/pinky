ec37fc234641cbecf38b9f69748c7ad3a09d0d65
HarshadaSom <49398228+HarshadaSom@users.noreply.github.com>
2019-04-09 13:54:32 +0530

    Update sample.java

--- demoGitProject/src/main/java/com/clc/java/gittest/demoGitProject/sample.java
+++ demoGitProject/src/main/java/com/clc/java/gittest/demoGitProject/sample.java
@@ -2,6 +2,6 @@ package com.clc.java.gittest.demoGitProject;
 
 public class sample {
 public static void main(String[] args) {
-	System.out.println("asdfghjuokijyrwqaghjhgf");
+	System.out.println("Welcome to git");
 }
 }
