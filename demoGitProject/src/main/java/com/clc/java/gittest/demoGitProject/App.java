3bbd34e43fb07fad81a8e4aedb3648e3c023f5cc
HarshadaSom <49398228+HarshadaSom@users.noreply.github.com>
2019-04-09 13:52:10 +0530

    Add files via upload

---
+++ demoGitProject/src/main/java/com/clc/java/gittest/demoGitProject/App.java
@@ -0,0 +1,13 @@
+package com.clc.java.gittest.demoGitProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ demoGitProject/src/main/java/com/clc/java/gittest/demoGitProject/sample.java
@@ -0,0 +1,7 @@
+package com.clc.java.gittest.demoGitProject;
+
+public class sample {
+public static void main(String[] args) {
+	System.out.println("asdfghjuokijyrwqaghjhgf");
+}
+}
